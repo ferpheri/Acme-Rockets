5eefe5c58d19eadd8f5b289909f0e8c39547bf0b
ferpheri <dehghanian77@gmail.com>
2024-05-19 12:33:15 +0330

    add theme toggle button and functionality

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,9 @@
 const initApp = () => {
     const hamburgerBtn = document.getElementById('hamburger-button')
     const mobileMenu = document.getElementById('mobile-menu')
+    const themeToggleBtn = document.getElementById('theme-toggle-btn')
+    const themeToggleIcon = document.getElementById('theme-toggle-icon')
+    const root = document.documentElement
 
     const toggleMenu = () => {
         mobileMenu.classList.toggle('hidden')
@@ -8,8 +11,20 @@ const initApp = () => {
         hamburgerBtn.classList.toggle('toggle-btn')
     }
 
+    const themeToggle = () => {
+        root.classList.toggle('dark')
+        if(root.classList.contains('dark')){
+            themeToggleIcon.textContent = 'dark_mode'
+        }else{
+            themeToggleIcon.textContent = 'light_mode'
+        }
+    }
+
     hamburgerBtn.addEventListener('click', toggleMenu)
     mobileMenu.addEventListener('click', toggleMenu)
+    themeToggleBtn.addEventListener('click', themeToggle)
+
 }
+
 
 document.addEventListener('DOMContentLoaded', initApp)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./build/**/*.{html,js}'],
+  darkMode :'class',
   theme: {
     extend: {
       screens: {
